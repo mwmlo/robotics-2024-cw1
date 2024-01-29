e3cbbae3fedb908f7c6449751343316937e2582a
Pi <rk1121@ic.ac.uk>
2024-01-29 12:29:52 +0000

    Roughly implement square

--- square.py
+++ square.py
@@ -3,6 +3,7 @@ from __future__ import division
 
 import time     # import the time library for the sleep function
 import brickpi3 # import the BrickPi3 drivers
+import math
 
 BP = brickpi3.BrickPi3()
 
@@ -10,27 +11,60 @@ RIGHT_WHEEL_PORT = BP.PORT_A
 LEFT_WHEEL_PORT = BP.PORT_D
 POWER_LIMIT = 70
 MAX_DPS = 200
-FORWARD_DPS = 180 # TODO: DETERMINE WHAT IS 40 CM
+FORWARD_DPS = 180
 TURN_DPS = 90
-# wheel radius = 2.5 CM
 
+TURN_ANGLE = 250
+FORWARD_DISTANCE = 850
+
+def start(l_angle_diff, r_angle_diff):
+    
+    initial_r = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
+    initial_l = BP.get_motor_encoder(LEFT_WHEEL_PORT)
+    
+    while True:
+
+        print("R:", BP.get_motor_status(RIGHT_WHEEL_PORT)) 
+        print("L:", BP.get_motor_status(LEFT_WHEEL_PORT)) 
+        
+        r_angle = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
+        l_angle = BP.get_motor_encoder(LEFT_WHEEL_PORT)
+        
+        if (math.abs(r_angle - initial_r) >= r_angle_diff or math.abs(l_angle - initial_l) >= l_angle_diff):
+            break;
+
+        time.sleep(1)
+
+def turn(angle = TURN_ANGLE):   
+    BP.set_motor_limits(RIGHT_WHEEL_PORT, POWER_LIMIT, TURN_DPS)
+    BP.set_motor_limits(LEFT_WHEEL_PORT, POWER_LIMIT, TURN_DPS)
+    
+    R_POS = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
+    L_POS = BP.get_motor_encoder(LEFT_WHEEL_PORT)
+    
+    BP.set_motor_position(RIGHT_WHEEL_PORT, R_POS + angle)
+    BP.set_motor_position(LEFT_WHEEL_PORT, L_POS - angle)
+    
+    start(L_POS - angle, R_POS + angle)
+    
+    
+def forward(distance = FORWARD_DISTANCE):
+    BP.set_motor_limits(RIGHT_WHEEL_PORT, POWER_LIMIT, MAX_DPS)
+    BP.set_motor_limits(LEFT_WHEEL_PORT, POWER_LIMIT, MAX_DPS)
+    
+    R_POS = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
+    L_POS = BP.get_motor_encoder(LEFT_WHEEL_PORT)
+
+    BP.set_motor_position(RIGHT_WHEEL_PORT, R_POS + distance)
+    BP.set_motor_position(LEFT_WHEEL_PORT, L_POS + distance)
+    
+    start(L_POS + distance, R_POS + distance)
+    
+    
 try:
-    BP.set_motor_limits(RIGHT_WHEEL_PORT, POWER_LIMIT, MAX_DPS)
-    BP.set_motor_limits(RIGHT_WHEEL_PORT, POWER_LIMIT, MAX_DPS)
-
-    # Go forwards
-    BP.set_motor_dps(RIGHT_WHEEL_PORT, FORWARD_DPS)
-    BP.set_motor_dps(LEFT_WHEEL_PORT, FORWARD_DPS)
-
-    time.sleep(1.5) # TODO: calibrate!
-
-    # Turn 90 degrees
-    BP.set_motor_dps(RIGHT_WHEEL_PORT, TURN_DPS)
-    BP.set_motor_dps(LEFT_WHEEL_PORT, -TURN_DPS)
-
-    time.sleep(0.02) # TODO: calibrate!
-
-
-
+    for i in range(4):
+        forward()
+        turn()
+        
 except KeyboardInterrupt: # program gets interrupted by Ctrl+C on the keyboard.
     BP.reset_all()
--- test.py
+++ test.py
@@ -1,2 +1,39 @@
-import brickpi3
-BP = brickpi3.BrickPi3()
+from __future__ import print_function # use python 3 syntax but make it compatible with python 2
+from __future__ import division 
+      
+import brickpi3         
+import time     # import the time library for the sleep function
+
+BP = brickpi3.BrickPi3() # Create an instance of the BrickPi3 class. BP will be the BrickPi3 object.
+
+try:
+    try:
+        BP.offset_motor_encoder(BP.PORT_A, BP.get_motor_encoder(BP.PORT_A)) # reset encoder A
+        BP.offset_motor_encoder(BP.PORT_D, BP.get_motor_encoder(BP.PORT_D)) # reset encoder D
+    except IOError as error:
+        print(error)
+        
+    BP.set_motor_limits(BP.PORT_A, 50)
+    BP.set_motor_limits(BP.PORT_D, 50) 
+    
+    n = 0
+    ang_a = 0
+    ang_d = 0
+    dt = 1
+    while n < 10:
+        sa = -72
+        sd = 72
+        BP.set_motor_dps(BP.PORT_A, sa)   
+        BP.set_motor_dps(BP.PORT_D, sd)
+        # optionally set a power limit
+        ang_a += sa*dt
+        ang_d += sd*dt
+        print("Motor A Status: ", BP.get_motor_status(BP.PORT_A), ", angle a:", ang_a, "; Motor D Status: ", BP.get_motor_status(BP.PORT_D), ", angle d:", ang_d)
+        
+        time.sleep(dt)
+        n += 1
+        
+    BP.reset_all()
+
+except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+    BP.reset_all()        # Unconfigure the sensors, disable the motors, and restore the LED to the control of the BrickPi3 firmware.
