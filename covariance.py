9bc22a26911174f1686bc02fc3bf2a1894870132
Michelle Lo <lomichelle42@gmail.com>
2024-01-31 11:41:33 +0000

    Measure

--- covariance.py
+++ covariance.py
@@ -1,6 +1,7 @@
 from statistics import mean
 
-locations = [] # e.g. [(x_1, y_1), (x_2, y_2), ..., (x_10, y_10)]
+locations = [(-3.0, 1.4), (-1.7, 2.0), (-0.6, 2.4), (0.9, 0.6), (0.4, 1.8), (1.7, -0.9), (0.9, 0.6), (0.7, 1.1), (-0.2, 2.4), (0.9, 0.6)] 
+# e.g. [(x_1, y_1), (x_2, y_2), ..., (x_10, y_10)]
 mean_x = mean([x for x, _ in locations])
 mean_y = mean([y for _, y in locations])
 cov_xx = mean([(x - mean_x) ** 2 for x, _ in locations])
