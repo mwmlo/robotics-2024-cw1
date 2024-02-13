ae79095046b8ffac62a7fd160a5bcb44fd202877
Michelle Lo <lomichelle42@gmail.com>
2024-02-13 16:15:00 +0000

    Accept visualizer as parameter

--- mcl_resample.py
+++ mcl_resample.py
@@ -1,22 +1,26 @@
 import random
 
-def normalise(particles):
-    total_weight = sum([p.weight for p in particles])
-    for p in particles:
+def normalize(visualizer):
+    """Normalize particle weights in visualizer."""
+    total_weight = sum([p.weight for p in visualizer.particles])
+    for p in visualizer.particles:
         p.weight /= total_weight
 
-def resample(particles):
+def resample(visualizer):
+    """Resamples and updates particle set."""
     cumulative_weights = []
     curr_total = 0
-    for p in particles:
+    for p in visualizer.particles:
         curr_total += p.weight
         cumulative_weights.append(curr_total)
+    # Resample particles based on cumulative weights
     resampled_particles = []
-    for _ in range(len(particles)):
+    for _ in range(len(visualizer.particles)):
         sample_num = random.uniform(0, 1)
         # See where the random number intersects with the array values
         for i, cw in enumerate(cumulative_weights):
             if sample_num >= cw:
-                resampled_particles.append(particles[i])
+                resampled_particles.append(visualizer.particles[i])
                 break
-    # TODO: overwrite the main arrays when resampling is done
+    # Overwrite the main arrays when resampling is done
+    visualizer.particles = resampled_particles
