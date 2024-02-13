e8792470e19f297d67ef6a4377800a80c5624451
Rishi Khiroya <rk1121@ic.ac.uk>
2024-02-13 16:21:39 +0000

    Move any non-visualize methods out of Visualize and into a new Robot class

--- Visualize.py
+++ Visualize.py
@@ -1,12 +1,10 @@
 import random
-import time
 
 import numpy as np
 
 from Constants import *
 from Particle import Particle
-from Utils import direction, ang_diff, deg_to_rad
-from main import BP, start
+from Utils import deg_to_rad
 
 
 class Visualize:
@@ -16,14 +14,16 @@ class Visualize:
         self.f_std = f_std
         self.g_std = g_std
         # generate list of particles
-        self.particles = [Particle(0, 0, 0, 1 / n_particles) for p in range(n_particles)]
+        self.particles = [Particle(0, 0, 0, 1 / n_particles) for _ in range(n_particles)]
         # draw axes
         self.draw_axes()
 
-    def gx(self, x):
+    @staticmethod
+    def __gx(x):
         return CENTER[0] + x * GRID_SCALE_FACTOR
 
-    def gy(self, y):
+    @staticmethod
+    def __gy(y):
         return CENTER[1] - y * GRID_SCALE_FACTOR
 
     def draw_axes(self):
@@ -34,14 +34,14 @@ class Visualize:
 
     def draw_line(self, line_coords):
         (x0, y0, x1, y1) = line_coords
-        print("drawLine:" + str((self.gx(x0), self.gy(y0), self.gx(x1), self.gy(y1))))
+        print("drawLine:" + str((self.__gx(x0), self.__gy(y0), self.__gx(x1), self.__gy(y1))))
 
     def draw_particles(self):
         particle_coords = [p.draw() for p in self.particles]
-        gparticle_coords = [(self.gx(x), self.gy(y), t) for (x, y, t) in particle_coords]
+        gparticle_coords = [(self.__gx(x), self.__gy(y), t) for (x, y, t) in particle_coords]
         print("drawParticles:" + str(gparticle_coords))
 
-    def loc_estimate(self):
+    def estimate_location(self):
         tot_w = 0
         locs = np.zeros(3)
         for p in self.particles:
@@ -50,26 +50,7 @@ class Visualize:
         mean_loc = locs / tot_w
         return mean_loc
 
-    def navigate(self, loc, x_targ, y_targ, draw=False):
-        vec = np.array([x_targ, y_targ]) - loc[:2]
-        distance = np.sqrt(pow(vec[0], 2) + pow(vec[1], 2))
-        rad = direction(vec[0], vec[1])
-        self.turn(ang_diff(loc[2], rad), draw)
-        time.sleep(0.5)
-        self.forward(distance, draw)
-
-    def turn(self, ang, draw=True):
-        angle = ang * TURN_PER_DEG
-        BP.set_motor_limits(RIGHT_WHEEL_PORT, POWER_LIMIT, TURN_DPS)
-        BP.set_motor_limits(LEFT_WHEEL_PORT, POWER_LIMIT, TURN_DPS)
-
-        R_POS = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
-        L_POS = BP.get_motor_encoder(LEFT_WHEEL_PORT)
-
-        BP.set_motor_position(RIGHT_WHEEL_PORT, R_POS + angle)
-        BP.set_motor_position(LEFT_WHEEL_PORT, L_POS - angle)
-
-        start(L_POS - angle, R_POS + angle)
+    def turn(self, ang, draw):
         rad = deg_to_rad(ang)
         for p in self.particles:
             g = random.gauss(0, self.g_std)
@@ -77,38 +58,11 @@ class Visualize:
         if draw:
             self.draw_particles()
 
-    def forward(self, dist, draw=True):
-        distance = dist * FORWARD_PER_CM
-        BP.set_motor_limits(RIGHT_WHEEL_PORT, POWER_LIMIT, MAX_DPS)
-        BP.set_motor_limits(LEFT_WHEEL_PORT, POWER_LIMIT, MAX_DPS)
-
-        R_POS = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
-        L_POS = BP.get_motor_encoder(LEFT_WHEEL_PORT)
-
-        BP.set_motor_position(RIGHT_WHEEL_PORT, R_POS + distance)
-        BP.set_motor_position(LEFT_WHEEL_PORT, L_POS + distance)
-
-        start(L_POS + distance, R_POS + distance)
+    def forward(self, dist, draw):
         for p in self.particles:
             e = random.gauss(0, self.e_std)
             f = random.gauss(0, self.f_std)
             p.update_line(dist, e, f)
+
         if draw:
             self.draw_particles()
-
-    def draw_square(self, size=40):
-        for i in range(4):
-            for j in range(4):
-                self.forward(size / 4)
-                time.sleep(0.5)
-            self.turn(90)
-            time.sleep(0.5)
-
-    def draw_star(self, size=30):
-        direction = 1
-        for i in range(5):
-            self.forward(direction * size)
-            time.sleep(0.5)
-            self.turn(36)
-            time.sleep(0.5)
-            direction = -direction
--- main.py
+++ main.py
@@ -4,35 +4,95 @@ from __future__ import print_function
 import time  # import the time library for the sleep function
 
 import brickpi3  # import the BrickPi3 drivers
+import numpy as np
 
 from Constants import *
-from Utils import deg_to_rad
+from Utils import deg_to_rad, direction, ang_diff
 from Visualize import Visualize
 
 BP = brickpi3.BrickPi3()
 
 
-def start(l_angle_target, r_angle_target, threshold=5, interval=0.5):
-    while True:
-        print("R:", BP.get_motor_status(RIGHT_WHEEL_PORT))
-        print("L:", BP.get_motor_status(LEFT_WHEEL_PORT))
+class Robot:
 
-        r_angle = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
-        l_angle = BP.get_motor_encoder(LEFT_WHEEL_PORT)
+    def __init__(self):
+        self.v = Visualize(0.2, deg_to_rad(1), deg_to_rad(3), 100)
 
-        if abs(r_angle - r_angle_target) <= threshold or abs(l_angle - l_angle_target) <= threshold:
-            break
+    @staticmethod
+    def start(l_angle_target, r_angle_target, threshold=5, interval=0.5):
+        while True:
+            print("R:", BP.get_motor_status(RIGHT_WHEEL_PORT))
+            print("L:", BP.get_motor_status(LEFT_WHEEL_PORT))
 
-        time.sleep(interval)
+            r_angle = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
+            l_angle = BP.get_motor_encoder(LEFT_WHEEL_PORT)
+
+            if abs(r_angle - r_angle_target) <= threshold or abs(l_angle - l_angle_target) <= threshold:
+                break
+
+            time.sleep(interval)
+
+    def navigate(self, loc, x_targ, y_targ, draw=False):
+        vec = np.array([x_targ, y_targ]) - loc[:2]
+        distance = np.sqrt(pow(vec[0], 2) + pow(vec[1], 2))
+        rad = direction(vec[0], vec[1])
+
+        self.turn(ang_diff(loc[2], rad), draw)
+        time.sleep(0.5)
+        self.forward(distance, draw)
+
+    def turn(self, ang, draw=True):
+        angle = ang * TURN_PER_DEG
+        BP.set_motor_limits(RIGHT_WHEEL_PORT, POWER_LIMIT, TURN_DPS)
+        BP.set_motor_limits(LEFT_WHEEL_PORT, POWER_LIMIT, TURN_DPS)
+
+        R_POS = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
+        L_POS = BP.get_motor_encoder(LEFT_WHEEL_PORT)
+
+        BP.set_motor_position(RIGHT_WHEEL_PORT, R_POS + angle)
+        BP.set_motor_position(LEFT_WHEEL_PORT, L_POS - angle)
+
+        self.start(L_POS - angle, R_POS + angle)
+        self.v.turn(ang, draw)
+
+    def forward(self, dist, draw=True):
+        distance = dist * FORWARD_PER_CM
+        BP.set_motor_limits(RIGHT_WHEEL_PORT, POWER_LIMIT, MAX_DPS)
+        BP.set_motor_limits(LEFT_WHEEL_PORT, POWER_LIMIT, MAX_DPS)
+
+        R_POS = BP.get_motor_encoder(RIGHT_WHEEL_PORT)
+        L_POS = BP.get_motor_encoder(LEFT_WHEEL_PORT)
+
+        BP.set_motor_position(RIGHT_WHEEL_PORT, R_POS + distance)
+        BP.set_motor_position(LEFT_WHEEL_PORT, L_POS + distance)
+
+        self.start(L_POS + distance, R_POS + distance)
+        self.v.forward(dist, draw)
+
+    def draw_square(self, size=40):
+        for i in range(4):
+            for j in range(4):
+                self.forward(size / 4)
+                time.sleep(0.5)
+            self.turn(90)
+            time.sleep(0.5)
+
+    def draw_star(self, size=30):
+        direction = 1
+        for i in range(5):
+            self.forward(direction * size)
+            time.sleep(0.5)
+            self.turn(36)
+            time.sleep(0.5)
+            direction = -direction
 
 
 if __name__ == "__main__":
     try:
-        v = Visualize(0.2, deg_to_rad(1), deg_to_rad(3), 100)
-        v.draw_square()
-        # v.draw_star(10)
-        # forward(40)
-        # turn(90)
+        robot = Robot()
+        # robot.draw_star(10)
+        # robot.forward(40)
+        # robot.turn(90)
 
     except KeyboardInterrupt:  # program gets interrupted by Ctrl+C on the keyboard.
         BP.reset_all()
