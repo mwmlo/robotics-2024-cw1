493a22ee01791757759b09721de3c76135003645
Michelle Lo <lomichelle42@gmail.com>
2024-02-20 21:54:33 +0000

    Change implementation of wall_distance to check valid walls

--- likelihood.py
+++ likelihood.py
@@ -1,29 +1,63 @@
 from Map import Map
 from Constants import LIKELIHOOD_K, LIKELIHOOD_STD
 import numpy as np
+import math
 
 den_lim = 0.001
 
-def wall_distance(x, y, theta, terrain: Map):
-    dmin = 255
-    for (x1, y1, x2, y2) in terrain.walls:
-        dy = y2 - y1
-        dx = x2 - x1
-        denom = dy * np.cos(theta) - dx * np.sin(theta)
-        if denom > den_lim or denom < -den_lim:
-            d = (dy * (x1 - x) - dx * (y1 - y)) / denom
-            if d >= 0 and d < dmin:
-                # check if this wall is valid
-                cross = np.array(x + d*np.cos(theta), y + d*np.sin(theta))
-                vec1 = np.array([x1, y1]) - cross
-                vec2 = np.array([x2, y2]) - cross
-                r = vec2 * vec1
-                #print("r: ", r)
-                if r[0] <= 0 or r[1] <= 0:
-                    # replace the result if current wall is closer
-                    dmin = d
+def vec_angle_from_east(x1, y1, x2, y2):
+    """Calculate the angle between the vector from (x1, y1) to (x2, y2) and the positive x-axis, measured counterclockwise from the east direction. Range [-pi, pi]."""
+    dx = x2 - x1
+    dy = y2 - y1
+    angle = math.atan2(dy, dx)
+    return angle
 
-    return dmin
+def will_hit_wall(pos_x, pos_y, pos_theta, wall):
+    """Return true if the robot's trajectory intersects with the given wall."""
+    x1, y1, x2, y2 = wall
+
+    # Calculate absolute angles for vectors from the robot to the wall's endpoints
+    angle1 = vec_angle_from_east(pos_x, pos_y, x1, y1)
+    angle2 = vec_angle_from_east(pos_x, pos_y, x2, y2)
+
+    # Check that the robot's orientation is within wall endpoints
+    return (angle1 <= pos_theta <= angle2) or (angle2 <= pos_theta <= angle1)
+
+def wall_distance(x, y, theta, terrain: Map):
+    """Returns the distance from the robot's position to the closest wall in the given Map."""
+    min_distance = 255 # If there is no nearby wall to hit, use maximum
+    for wall in terrain.walls:
+        if will_hit_wall(x, y, theta, wall):
+            x1, y1, x2, y2 = wall
+            dy = y2 - y1
+            dx = x2 - x1
+            numerator = (dy * (x1 - x) - dx * (y1 - y))
+            denominator = dy * np.cos(theta) - dx * np.sin(theta)
+            distance = numerator / denominator
+            if distance >= 0:
+                min_distance = min(distance, min_distance)
+    return min_distance
+
+# def wall_distance(x, y, theta, terrain: Map):
+#     dmin = 255
+#     for (x1, y1, x2, y2) in terrain.walls:
+#         dy = y2 - y1
+#         dx = x2 - x1
+#         denom = dy * np.cos(theta) - dx * np.sin(theta)
+#         if denom > den_lim or denom < -den_lim:
+#             d = (dy * (x1 - x) - dx * (y1 - y)) / denom
+#             if d >= 0 and d < dmin:
+#                 # check if this wall is valid
+#                 cross = np.array(x + d*np.cos(theta), y + d*np.sin(theta))
+#                 vec1 = np.array([x1, y1]) - cross
+#                 vec2 = np.array([x2, y2]) - cross
+#                 r = vec2 * vec1
+#                 #print("r: ", r)
+#                 if r[0] <= 0 or r[1] <= 0:
+#                     # replace the result if current wall is closer
+#                     dmin = d
+
+#     return dmin
 
 
 def likelihood(d_measure, d_true):
--- mcl_tests.py
+++ mcl_tests.py
@@ -1,8 +1,9 @@
 import unittest
 
+import numpy as np
 from Utils import deg_to_rad, direction, ang_diff
 from Visualize import Visualize
-from likelihood import wall_distance, likelihood
+from likelihood import *
 from mcl_resample import normalize, resample
 from Map import Map, myMap
 # from main import Robot
@@ -14,6 +15,37 @@ class TestMonteCarloMethods(unittest.TestCase):
         self.visualizer = Visualize(0.2, deg_to_rad(1), deg_to_rad(3))
         self.terrain = myMap(self.visualizer)
 
+    # Check that absolute angle relative to x-axis is correctly calculated
+    def test_vec_angle_from_east(self):
+        northwest = vec_angle_from_east(0, 0, -10, 10)
+        self.assertGreaterEqual(northwest, np.pi / 2)
+        self.assertLessEqual(northwest, np.pi)
+
+        southeast = vec_angle_from_east(0, 0, 10, -10)
+        self.assertGreaterEqual(southeast, -np.pi / 2)
+        self.assertLessEqual(southeast, 0)
+        
+        northeast = vec_angle_from_east(0, 0, 10, 10)
+        self.assertGreaterEqual(northeast, 0)
+        self.assertLessEqual(northeast, np.pi / 2)
+        
+        southwest = vec_angle_from_east(0, 0, -10, -10)
+        self.assertGreaterEqual(southwest, -np.pi)
+        self.assertLessEqual(southwest, -np.pi / 2)
+
+    def test_will_hit_wall_wp1_orientx(self):
+        GH_wall = (210, 84, 210, 0)
+        self.assertTrue(will_hit_wall(84, 30, 0, GH_wall))
+        # Although OH is closer, the robot is not facing OH so it should not hit
+        OH_wall = (0, 0, 210, 0)
+        self.assertFalse(will_hit_wall(84, 30, 0, OH_wall))
+
+    def test_will_hit_wall_corner(self):
+        AB_wall = (0, 168, 84, 168)
+        OA_wall = (0, 0, 0, 168)
+        self.assertTrue(will_hit_wall(10, 158, 3 * np.pi/4, AB_wall))
+        self.assertTrue(will_hit_wall(10, 158, 3 * np.pi/4, OA_wall))
+
     def test_wall_distance_wp1_orientx(self):
         # Location: waypoint 1, facing along the x-axis (right)
         pos_x, pos_y, pos_theta = 84, 30, 0
@@ -24,7 +56,7 @@ class TestMonteCarloMethods(unittest.TestCase):
 
     def test_wall_distance_wp1_orienty(self):
         # Location: waypoint 1, facing along the y-axis (down)
-        pos_x, pos_y, pos_theta = 84, 30, -90
+        pos_x, pos_y, pos_theta = 84, 30, (-np.pi/2)
         # Should be facing wall between points O, H
         expected_distance = abs(0 - pos_y)
         predicted_distance = wall_distance(pos_x, pos_y, pos_theta, self.terrain)
@@ -40,7 +72,7 @@ class TestMonteCarloMethods(unittest.TestCase):
 
     def test_wall_distance_wp2_orienty(self):
         # Location: waypoint 2, facing along the y-axis (up)
-        pos_x, pos_y, pos_theta = 180, 30, 90
+        pos_x, pos_y, pos_theta = 180, 30, (np.pi/2)
         # Should be facing wall between points F, G
         expected_distance = 84 - pos_y
         predicted_distance = wall_distance(pos_x, pos_y, pos_theta, self.terrain)
